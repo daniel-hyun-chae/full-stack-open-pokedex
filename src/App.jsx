71e6275b3af63e52d785e3688b50d410ea06e2ea
Hyun Chae <daniel.hyun.chae@gmail.com>
2023-02-01 14:54:50 +0100

    #skip test

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import ErrorMessage from "./ErrorMessage";
 import PokemonPage from "./PokemonPage";
 import PokemonList from "./PokemonList";
 
+// test
 const mapResults = ({ results }) =>
   results.map(({ url, name }) => ({
     url,
