b7aea237b62460301b393bedca39a101c6d2b875
Hyun Chae <daniel.hyun.chae@gmail.com>
2023-02-01 13:35:01 +0100

    Testing pipeline

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,6 @@ import ErrorMessage from "./ErrorMessage";
 import PokemonPage from "./PokemonPage";
 import PokemonList from "./PokemonList";
 
-//test comment
-//more comment
-
 const mapResults = ({ results }) =>
   results.map(({ url, name }) => ({
     url,
